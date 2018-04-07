c2d9be86f9356669b0783a27bd730c641a823a78
Tanaphon Auanhinkong <a.tanaphon@gmail.com>
2018-04-07 17:32:32 +0700

    match media

--- conditionally/index.js
+++ conditionally/index.js
@@ -1,4 +1,13 @@
 (function () {
+  const isLandScape = matchMedia('(orientation: landscape)')
 
-  
+  const toggleClass = (mq) => {
+    if (mq.matches) {
+      document.body.style.background = '#fff'
+    } else {
+      document.body.style.background = '#000'
+    }
+  }
+
+  isLandScape.addListener(toggleClass)
 })()
